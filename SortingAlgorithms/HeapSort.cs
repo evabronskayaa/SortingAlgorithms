728c11a24dde5588a7a6426ccc26df8fc46e686d
evabronskayaa <eva27122002b@gmail.com>
2021-11-18 15:14:39 +0500

    added ShellSort & HeapSort

---
+++ SortingAlgorithms/HeapSort.cs
@@ -0,0 +1,43 @@
+﻿namespace SortingAlgorithms
+{
+    public static class HeapSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--) 
+            {
+                Heapify(arr, n, i);
+            }
+            
+            for (int i=n-1; i>=0; i--) 
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
+        }
+
+        private static void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] > arr[largest]) largest = left;
+            if (right < n && arr[right] > arr[largest]) largest = right;
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                Heapify(arr, n, largest);
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/ShellSort.cs
@@ -0,0 +1,33 @@
+﻿namespace SortingAlgorithms
+{
+    public static class ShellSort
+    {
+        private static void Swap(ref int a, ref int b)
+        {
+            var t = a;
+            a = b;
+            b = t;
+        }
+
+        public static int[] Sort(int[] array)
+        {
+            var d = array.Length / 2;
+            while (d >= 1)
+            {
+                for (var i = d; i < array.Length; i++)
+                {
+                    var j = i;
+                    while ((j >= d) && (array[j - d] > array[j]))
+                    {
+                        Swap(ref array[j], ref array[j - d]);
+                        j -= d;
+                    }
+                }
+
+                d /= 2;
+            }
+
+            return array;
+        }
+    }
+}
