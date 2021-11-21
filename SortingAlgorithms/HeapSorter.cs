e0942b77610ef98858f21a4a1aaa9658f1db124a
nodlya <nadya-popova-02@mail.ru>
2021-11-21 19:03:20 +0500

    added generics and string overload

--- SortingAlgorithms/HeapSorter.cs
+++ SortingAlgorithms/HeapSorter.cs
@@ -1,4 +1,6 @@
-﻿namespace SortingAlgorithms
+﻿using System;
+
+namespace SortingAlgorithms
 {
     public static class HeapSorter
     {
@@ -25,6 +27,42 @@
 
             if (left < n && array[left] > array[largest]) largest = left;
             if (right < n && array[right] > array[largest]) largest = right;
+
+            if (largest != i)
+            {
+                var swap = array[i];
+                array[i] = array[largest];
+                array[largest] = swap;
+
+                Heapify(array, n, largest);
+            }
+        }
+
+        public static void HeapSort<T>(this T[] array)
+            where T: IComparable
+        {
+            var n = array.Length;
+            for (var i = n / 2 - 1; i >= 0; i--) Heapify(array, n, i);
+
+            for (var i = n - 1; i >= 0; i--)
+            {
+                var temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+
+                Heapify(array, i, 0);
+            }
+        }
+
+        private static void Heapify<T>(T[] array, int n, int i)
+            where T: IComparable
+        {
+            var largest = i;
+            var left = 2 * i + 1;
+            var right = 2 * i + 2;
+
+            if (left < n && array[left].CompareTo(array[largest])>0) largest = left;
+            if (right < n && array[right].CompareTo(array[largest])>0) largest = right;
 
             if (largest != i)
             {
--- SortingAlgorithms/ShellSorter.cs
+++ SortingAlgorithms/ShellSorter.cs
@@ -1,4 +1,6 @@
-﻿namespace SortingAlgorithms
+﻿using System;
+
+namespace SortingAlgorithms
 {
     public static class ShellSorter
     {
@@ -28,6 +30,53 @@
             }
 
             return array;
+        }
+
+
+
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            var t = a;
+            a = b;
+            b = t;
+        }
+        public static T[] ShellSort<T>(this T[] array)
+            where T: IComparable
+        {
+            var d = array.Length / 2;
+            while (d >= 1)
+            {
+                for (var i = d; i < array.Length; i++)
+                {
+                    var j = i;
+                    while ((j >= d) && (array[j - d].CompareTo(array[j]))>0)
+                    {
+                        Swap(ref array[j], ref array[j - d]);
+                        j -= d;
+                    }
+                }
+
+                d /= 2;
+            }
+
+            return array;
+        }
+
+        public static int CompareStrings(string a,string b)
+        {
+            a = a.ToLower();
+            b = b.ToLower();
+            int length = a.Length > b.Length ? b.Length : a.Length;
+            for (int i=0;i<length;i++)
+            {
+                if (a[i] == b[i]) continue;
+                if (a[i] < b[i]) return 0;
+                else return 1;
+            }
+
+            if (a.Length < b.Length) return 0;
+            else return 1;
         }
     }
 }
