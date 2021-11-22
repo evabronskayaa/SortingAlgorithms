835ad63af5821a890782f302132eed5366d7b6ab
evabronskayaa <eva27122002b@gmail.com>
2021-11-22 16:30:00 +0500

    added Program.cs

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = {12, 11, 13, 5, 6, 7};
+
+            arr.ShellSort();
+            //HeapSort.Sort(arr);
+
+            Console.WriteLine("Sorted array is");
+            PrintArray(arr);
+        }
+        
+        private static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i=0; i<n; ++i)
+                Console.Write(arr[i]+" ");
+        }
+
+    }
+}
