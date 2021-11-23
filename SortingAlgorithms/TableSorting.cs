8b33b8d56ce96da469d9ca4b3061ef12bc768fb4
MindHardt <71509337+MindHardt@users.noreply.github.com>
2021-11-24 00:11:33 +0500

    Added a raw template for table sorting

---
+++ SortingAlgorithms/TableSorting.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SortingAlgorithms
+{
+    class TableSorting
+    {
+
+    }
+
+    public class Table
+    {
+        public Row[] Rows;
+        public Column[] Columns;
+
+        public class Row
+        {
+            public readonly int ID;
+            public string[] Elements;
+            public Row(string[] elems)
+            {
+                Elements = elems;
+            }
+        }
+        public class Column
+        {
+            public readonly string Name;
+            public string[] Elements;
+            public Column(string name)
+            {
+                Name = name;
+            }
+        }
+
+        public string this[int indexRow, int indexCol]
+        {
+            get => Rows[indexRow].Elements[indexCol];
+            set
+            {
+                Rows[indexRow].Elements[indexCol] = value;
+                Columns[indexCol].Elements[indexRow] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string header = Columns.Select(c => c.Name).Aggregate((c1, c2) => c1 + "    " + c2);
+
+            StringBuilder sb = new StringBuilder(header);
+
+            foreach (var row in Rows)
+            {
+                sb.Append(row.Elements.Aggregate((e1, e2) => e1 + "    " + e2) + "\n");
+            }
+
+            return sb.ToString();
+        }
+
+        public static FileStream ConvertToCSV(string pathToFile)
+        {
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(pathToFile);
+            wb.SaveAs(@"C:\Temp\output.csv", Excel.XlFileFormat.xlCSVWindows);
+            wb.Close(false);
+            app.Quit();
+        }
+
+        public Table(string pathToCSV)
+        {
+            var lines = File.ReadAllLines(pathToCSV);
+
+            Columns = lines[0].Split(';').Select(s => new Column(s)).ToArray();
+            Rows = new Row[lines.Length - 1].Select(r => new Row(new string[Columns.Length])).ToArray();
+
+            var elements = lines[1..].Select(s => s.Split(';')).ToArray();
+
+            for (int row = 0; row < Rows.Length; row++)
+            {
+                for (int col = 0; col < Columns.Length; col++)
+                {
+                    this[row, col] = elements[row][col];
+                }
+            }    
+        }
+    }
+}
